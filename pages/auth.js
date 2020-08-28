dfb9f33f0e14c810184ce1cb07dee2989183470a
Nicholas Herrick <nth.herrick@gmail.com>
2020-08-28 13:41:09 -0600

    created authcontext and authservice file

--- pages/auth.js
+++ pages/auth.js
@@ -1,12 +1,67 @@
 import Layout from '../components/Layout';
+import { useState, useContext } from 'react';
+import AuthService from '../services/AuthService';
+import { AuthContext } from '../context/AuthContext';
 
-export default function Auth() {
+const Auth = (props) => {
+  const [user, setUser] = useState({ email: '', password: '' });
+  const [message, setMessage] = useState(null);
+  const authContext = useContext(AuthContext);
+
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    AuthService.login(user).then((data) => {
+      const { isAuthenticated, user, message } = data;
+      if (isAuthenticated) {
+        authContext.setUser(user);
+        authContext.setIsAuthenticated(isAuthenticated);
+        props.history.push('/todos');
+      } else {
+        setMessage(message);
+      }
+    });
+  };
+
   return (
     <Layout>
       <div>
         <h1>Welcome to Mural Map</h1>
         <h2>Login/Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className='col-md-6 mx-auto'>
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                name='email'
+                onChange={handleChange}
+                className='form-control'
+                placeholder='email@example.com'
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                name='password'
+                onChange={handleChange}
+                className='form-control'
+                placeholder='Enter password'
+              />
+            </div>
+            <button className='btn btn-lg btn-primary btn-block' type='submit'>
+              Login
+            </button>
+            {/* {message ? <Message message={message} /> : null} */}
+          </div>
+        </form>
       </div>
     </Layout>
   );
-}
+};
+
+export default Auth;
---
+++ services/AuthService.js
@@ -0,0 +1,38 @@
+export default {
+  register: (user) => {
+    return fetch('/api/auth/register', {
+      method: 'post',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.status !== 401) return res.json().then((data) => data);
+      else return { isAuthenticated: false, user: { email: '', username: '' } };
+    });
+  },
+  login: (user) => {
+    return fetch('/api/auth/login', {
+      method: 'post',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.status !== 401) return res.json().then((data) => data);
+      else return { isAuthenticated: false, user: { email: '', username: '' } };
+    });
+  },
+  logout: () => {
+    return fetch('/api/auth/logout')
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+  // Sync back-end and front-end so session persists even if window closes
+  isAuthenticated: () => {
+    return fetch('/api/auth/authenticated').then((res) => {
+      if (res.status !== 401) return res.json().then((data) => data);
+      else return { isAuthenticated: false, user: { email: '', username: '' } };
+    });
+  },
+};
