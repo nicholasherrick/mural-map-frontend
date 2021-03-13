8c32e07500220cfdc0e9ad44edfc917afae55e3b
Nicholas Herrick <nth.herrick@gmail.com>
2021-03-13 13:47:13 -0700

    add loader component and implement it in _app.js

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,32 +1,32 @@
 import React, { createContext, useState, useEffect } from 'react';
 import AuthService from '../services/AuthService';
+import Loader from '../components/Loader';
 
 export const AuthContext = createContext();
 
 export default ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoaded, setIsLoaded] = useState(false);
+    const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    AuthService.isAuthenticated().then((data) => {
-      setUser(data.user);
-      setIsAuthenticated(data.isAuthenticated);
-      setIsLoaded(true);
-    });
-  }, []);
+    useEffect(() => {
+        AuthService.isAuthenticated().then((data) => {
+            setUser(data.user);
+            setIsAuthenticated(data.isAuthenticated);
+            setIsLoaded(true);
+        });
+    }, []);
 
-  return (
-    <div>
-      {!isLoaded ? (
-        <h1>Loading</h1>
-      ) : (
-        <AuthContext.Provider
-          value={{ user, setUser, isAuthenticated, setIsAuthenticated }}
-        >
-          {children}
-        </AuthContext.Provider>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            {!isLoaded ? (
+                <Loader />
+            ) : (
+                <AuthContext.Provider
+                    value={{ user, setUser, isAuthenticated, setIsAuthenticated }}>
+                    {children}
+                </AuthContext.Provider>
+            )}
+        </div>
+    );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,29 @@
 import '../styles/globals.scss';
 import '@reach/combobox/styles.css';
+import React, { useState, useEffect } from 'react';
 import AuthProvider from '../context/AuthContext';
+import { useRouter } from 'next/router';
+import Loader from '../components/Loader';
 
 function MyApp({ Component, pageProps }) {
-    return (
+    const router = useRouter();
+    const [pageLoading, setPageLoading] = useState(false);
+    useEffect(() => {
+        const handleStart = () => {
+            setPageLoading(true);
+        };
+        const handleComplete = () => {
+            setPageLoading(false);
+        };
+
+        router.events.on('routeChangeStart', handleStart);
+        router.events.on('routeChangeComplete', handleComplete);
+        router.events.on('routeChangeError', handleComplete);
+    }, [router]);
+
+    return pageLoading ? (
+        <Loader />
+    ) : (
         <AuthProvider>
             <Component {...pageProps} />
         </AuthProvider>
